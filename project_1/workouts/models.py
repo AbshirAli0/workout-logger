f1bee7cf209a3d4e2fbadcd2dec08882f7525239
AbshirAli0 <abshirali1001@gmail.com>
2026-01-17 15:21:13 -0500

    add exercise/sets/reps fields

--- project_1/workouts/models.py
+++ project_1/workouts/models.py
@@ -4,14 +4,18 @@ from django.conf import settings
 # Create your models here.
 
 class Workout(models.Model):
-    
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="workouts"
     )
 
-    def __str__(self):
-        return f"Workout by {self.user}"
+    exercise = models.CharField(max_length=100)
+    sets = models.IntegerField(default = 0)
 
+    def __str__(self):
+        return f"{self.exercise} by {self.user}"
+    
+   
     
