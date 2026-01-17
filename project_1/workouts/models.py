4e66447c2c646b105e767918d427a711d84a3c4b
AbshirAli0 <abshirali1001@gmail.com>
2026-01-17 15:10:19 -0500

    added Workout model Foreign key

--- project_1/workouts/models.py
+++ project_1/workouts/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 class Workout(models.Model):
-    pass
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="workouts"
+    )
+
+    def __str__(self):
+        return f"Workout by {self.user}"
+
+    
