e2fa41398f69a1864fbe3d18031ba45f0bb6c3b3
AbshirAli0 <abshirali1001@gmail.com>
2026-01-19 11:15:02 -0500

    Add Workout admin configuration

--- project_1/workouts/admin.py
+++ project_1/workouts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Workout
 
 # Register your models here.
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ("exercise","sets","created_at",)
+
+
