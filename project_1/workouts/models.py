9ede5d4297d32349c4b64c4337b6195ff6776905
AbshirAli0 <abshirali1001@gmail.com>
2026-01-20 16:34:19 -0500

    Apply migration to update workout model

--- project_1/workouts/models.py
+++ project_1/workouts/models.py
@@ -12,9 +12,11 @@ class Workout(models.Model):
     )
 
     exercise = models.CharField(max_length=100)
+    weight = models.IntegerField(default = 0)
     sets = models.IntegerField(default = 0)
+    reps = models.IntegerField(default = 0)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"{self.exercise} by {self.user}"
     
